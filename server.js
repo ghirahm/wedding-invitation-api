ae8cd090773fb2935ebba193b4dee6c3ff38c826
Ghirah Madani <ghirahmadanis@gmail.com>
2025-02-06 16:13:18 +0700

    Update Features Fix Bug

--- server.js
+++ server.js
@@ -36,5 +36,5 @@ app.get("/messages/:chat_id", async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
